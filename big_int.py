ec3abb8a14e973cc7c3a38699d0af74ab71cd45a
sshrik <chj878194@naver.com>
2020-08-16 18:34:15 +0900

    big_int

---
+++ big_int.py
@@ -0,0 +1,14 @@
+class BigInteger():
+    def __init__(self, number):
+        self.value = number
+    
+    def __mod__(self, other):
+
+
+    def __add__(self, other):
+        
+
+    def __sub__(self, other):
+
+
+    def __truediv__(self, other):
