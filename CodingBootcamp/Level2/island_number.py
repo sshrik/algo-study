ef5332053be1ab08cd0153da5892ee0fbde50a3a
sshrik <chj8781942naver.com>
2020-09-17 17:01:24 +0900

    Level2 finish.

--- CodingBootcamp/Level2/island_number.py
+++ CodingBootcamp/Level2/island_number.py
@@ -3,3 +3,65 @@
 # [1, 1, 0, 0, 0]
 # [0, 0, 0, 0, 0]
 # 1은 땅, 0이 바다인 경우, 섬의 갯수를 찾아라.
+
+
+import sys
+
+LAND = 1
+OCEAN = 0
+
+# 동 서 남 북으로 이동시 x, y, 의 변화
+x_diff = [0, 0, 1, -1]
+y_diff = [1, -1, 0, 0]
+
+def dfs(inp_map, visited, n, m, x, y):
+    # Depth first seach for starting (x, y).
+    next_queue = [(x, y)]
+
+    while len(next_queue) != 0:
+        # Check now check coordinate.
+        (now_x, now_y) = next_queue[0]
+        del next_queue[0]
+        # 이미 방문한 곳이면 가지 않는다.
+        if visited[now_x][now_y]:
+            continue
+
+        visited[now_x][now_y] = True
+
+        # 모든 방향에 대해서, 갈 수 있는 경우를 검색.
+        for direction in range(0, 4):
+            if can_go(n, m, now_x, now_y, direction):
+                # 방문하지 않았던 땅 일 경우에만 Queue 에 추가한다.
+                if inp_map[now_x + x_diff[direction]][now_y + y_diff[direction]] == LAND and not visited[now_x + x_diff[direction]][now_y + y_diff[direction]]:
+                    next_queue.append((now_x + x_diff[direction], now_y + y_diff[direction]))
+
+def can_go(n, m, x, y, direction):
+    return x + x_diff[direction] < n and x + x_diff[direction] > -1 and y + y_diff[direction] < m and y + y_diff[direction] > -1
+
+
+if __name__ == "__main__":
+    inp = sys.stdin.readline().strip().split(" ")
+    n = int(inp[0])
+    m = int(inp[1])
+
+    inp_map = []
+    visited = []
+
+    for _ in range(0, n):
+        inp_map.append([])
+        visited.append([])
+        inp = sys.stdin.readline().strip().split(" ")
+        for i in range(0, m):
+            inp_map[-1].append(int(inp[i]))
+            visited[-1].append(False)
+
+    continent = 0
+
+    for x in range(0, n):
+        for y in range(0, m):
+            if inp_map[x][y] == LAND and not visited[x][y]: 
+                dfs(inp_map, visited, n, m, x, y)
+                continent += 1
+
+
+    print(continent)
--- CodingBootcamp/Level2/merge_diff.py
+++ CodingBootcamp/Level2/merge_diff.py
@@ -1,13 +1,30 @@
-# [1, 3], [2, 6], [8, 10], [15, 18]의 범위들이 입력됨.
+# [1, 3], [2, 6], [8, 10], [15, 18]의 범위들이 입력됨. ( 작은게 앞에 온다고 가정 )
 # 1 ~ 18 중에서 한번이라도 겹쳐지는 부분을 찾아내야 한다.
 # [1, 6], [8, 10], [15, 18]
 
 import sys
 
+def merge_diff(diff1, diff2):
+    small_start = diff1[0] if diff1[0] < diff2[0] else diff2[0]
+    large_end = diff2[1] if diff1[1] < diff2[1] else diff1[1]
+
+    return (small_start, large_end)
+
+def is_contain(diff1, diff2):
+    # 2개의 범위가 겹치는 부분이 있는가?
+    small_start = diff1 if diff1[0] < diff2[0] else diff2
+    large_start = diff2 if diff1[0] < diff2[0] else diff1
+    
+    return small_start[1] >= large_start[0]
+    
 if __name__ == "__main__":
     n = int(sys.stdin.readline().rstrip())
     input_set = []
     for _ in range(0, n):
         inp = sys.stdin.readline().rstrip().split(" ")
         input_set.append((int(inp[0]), int(inp[1])))    # Add diff set.
-        
+
+    input_set.sort()
+
+    result_diff = [input_set[0]]
+    
